e157d14029127ae901f83583764d090e3f0b46b0
Kirill <kirill.parakhnevich@mail.ru>
2019-11-20 13:19:28 +0300

    Add project files.

---
+++ BusSimulator/frmMain.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace BusSimulator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.tBxFileAddress = new System.Windows.Forms.TextBox();
+            this.gpBxData = new System.Windows.Forms.GroupBox();
+            this.gpBxAction = new System.Windows.Forms.GroupBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.cBxFinish = new System.Windows.Forms.ComboBox();
+            this.cBxTime = new System.Windows.Forms.ComboBox();
+            this.cBxStart = new System.Windows.Forms.ComboBox();
+            this.btnFindRoutes = new System.Windows.Forms.Button();
+            this.gpBxData.SuspendLayout();
+            this.gpBxAction.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(417, 415);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(357, 45);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(123, 23);
+            this.btnLoadFile.TabIndex = 1;
+            this.btnLoadFile.Text = "Загрузить файл";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // tBxFileAddress
+            // 
+            this.tBxFileAddress.Location = new System.Drawing.Point(6, 19);
+            this.tBxFileAddress.Name = "tBxFileAddress";
+            this.tBxFileAddress.Size = new System.Drawing.Size(474, 20);
+            this.tBxFileAddress.TabIndex = 2;
+            // 
+            // gpBxData
+            // 
+            this.gpBxData.Controls.Add(this.btnLoadFile);
+            this.gpBxData.Controls.Add(this.tBxFileAddress);
+            this.gpBxData.Location = new System.Drawing.Point(12, 12);
+            this.gpBxData.Name = "gpBxData";
+            this.gpBxData.Size = new System.Drawing.Size(486, 81);
+            this.gpBxData.TabIndex = 4;
+            this.gpBxData.TabStop = false;
+            this.gpBxData.Text = "Путь к файлу:";
+            // 
+            // gpBxAction
+            // 
+            this.gpBxAction.Controls.Add(this.lblResult);
+            this.gpBxAction.Controls.Add(this.lbl3);
+            this.gpBxAction.Controls.Add(this.lbl2);
+            this.gpBxAction.Controls.Add(this.lbl1);
+            this.gpBxAction.Controls.Add(this.cBxFinish);
+            this.gpBxAction.Controls.Add(this.cBxTime);
+            this.gpBxAction.Controls.Add(this.cBxStart);
+            this.gpBxAction.Controls.Add(this.btnFindRoutes);
+            this.gpBxAction.Location = new System.Drawing.Point(12, 99);
+            this.gpBxAction.Name = "gpBxAction";
+            this.gpBxAction.Size = new System.Drawing.Size(486, 310);
+            this.gpBxAction.TabIndex = 5;
+            this.gpBxAction.TabStop = false;
+            this.gpBxAction.Text = "Поиск путей:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.Location = new System.Drawing.Point(16, 138);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(94, 36);
+            this.lblResult.TabIndex = 10;
+            this.lblResult.Text = "Результат:\r\n-\r\n";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(241, 25);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(108, 13);
+            this.lbl3.TabIndex = 9;
+            this.lbl3.Text = "Время отправления";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(135, 25);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(86, 13);
+            this.lbl2.TabIndex = 8;
+            this.lbl2.Text = "Конечная точка";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(16, 25);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(93, 13);
+            this.lbl1.TabIndex = 7;
+            this.lbl1.Text = "Начальная точка";
+            // 
+            // cBxFinish
+            // 
+            this.cBxFinish.FormattingEnabled = true;
+            this.cBxFinish.Location = new System.Drawing.Point(132, 41);
+            this.cBxFinish.Name = "cBxFinish";
+            this.cBxFinish.Size = new System.Drawing.Size(98, 21);
+            this.cBxFinish.TabIndex = 6;
+            this.cBxFinish.SelectedIndexChanged += new System.EventHandler(this.cBxFinish_SelectedIndexChanged);
+            // 
+            // cBxTime
+            // 
+            this.cBxTime.FormattingEnabled = true;
+            this.cBxTime.Location = new System.Drawing.Point(244, 41);
+            this.cBxTime.Name = "cBxTime";
+            this.cBxTime.Size = new System.Drawing.Size(98, 21);
+            this.cBxTime.TabIndex = 5;
+            // 
+            // cBxStart
+            // 
+            this.cBxStart.FormattingEnabled = true;
+            this.cBxStart.Location = new System.Drawing.Point(16, 41);
+            this.cBxStart.Name = "cBxStart";
+            this.cBxStart.Size = new System.Drawing.Size(98, 21);
+            this.cBxStart.TabIndex = 4;
+            this.cBxStart.SelectedIndexChanged += new System.EventHandler(this.cBxStart_SelectedIndexChanged);
+            // 
+            // btnFindRoutes
+            // 
+            this.btnFindRoutes.Location = new System.Drawing.Point(405, 281);
+            this.btnFindRoutes.Name = "btnFindRoutes";
+            this.btnFindRoutes.Size = new System.Drawing.Size(75, 23);
+            this.btnFindRoutes.TabIndex = 3;
+            this.btnFindRoutes.Text = "Поиск";
+            this.btnFindRoutes.UseVisualStyleBackColor = true;
+            this.btnFindRoutes.Click += new System.EventHandler(this.btnFindRoutes_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 450);
+            this.Controls.Add(this.gpBxAction);
+            this.Controls.Add(this.gpBxData);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmMain";
+            this.Text = "BusSimulator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.gpBxData.ResumeLayout(false);
+            this.gpBxData.PerformLayout();
+            this.gpBxAction.ResumeLayout(false);
+            this.gpBxAction.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.TextBox tBxFileAddress;
+        private System.Windows.Forms.GroupBox gpBxData;
+        private System.Windows.Forms.GroupBox gpBxAction;
+        private System.Windows.Forms.Button btnFindRoutes;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.ComboBox cBxFinish;
+        private System.Windows.Forms.ComboBox cBxTime;
+        private System.Windows.Forms.ComboBox cBxStart;
+    }
+}
+
---
+++ BusSimulator/frmMain.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using QuickGraph;
+using QuickGraph.Algorithms;
+
+namespace BusSimulator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        Simulation sim = new Simulation();
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            if (cBxStart.Items.Count > 0) //При загрузке нового файла
+            {
+                cBxTime.Items.Clear();
+                cBxStart.Items.Clear();
+                cBxFinish.Items.Clear();
+                lblResult.Text = "Результат:\n-";
+            }
+
+            //Чтение файла
+            var lines = File.ReadAllLines(tBxFileAddress.Text);
+            int busCount = Convert.ToInt32(lines[0]);
+            int busStops = Convert.ToInt32(lines[1]);
+
+            //Основной объект
+            sim = new Simulation
+            {
+                StopCount = busStops
+            };
+
+            string[] startTimes = lines[2].Split(' ');
+            string[] ticketCosts = lines[3].Split(' ');
+
+            for (int i = 1; i <= busStops; i++)
+            {
+                cBxStart.Items.Add(i.ToString());
+                cBxFinish.Items.Add(i.ToString());
+            }
+
+            //Заполнение массива автобусов
+            for (int i = 0; i < busCount; i++)
+            {
+                Bus bus = new Bus
+                {
+                    StartTime = startTimes.ElementAt(i),
+                    TicketCost = Convert.ToInt32(ticketCosts.ElementAt(i)),
+                    ID = i + 1
+                };
+
+                sim.Buses.Add(bus);
+            }
+
+            //Составление и заполнение расписаний автобусов
+            for (int i = 4; i < lines.Length; i++)
+            {
+                string tempo = lines[i];
+                int count = Convert.ToInt32(tempo.Substring(0, tempo.IndexOf(' ')));
+                tempo = tempo.Substring(tempo.IndexOf(' ') + 1);
+                List<int> stopData = tempo.Split(' ').Select(Int32.Parse).ToList();
+
+                //DateTime startTime = Convert.ToDateTime(sim.Buses[i - 4].StartTime);
+                for (int j = 0; j < count; j++)
+                {
+                    BusStop busStop = new BusStop
+                    {
+                        Number = stopData[j],
+                        TravelTime = stopData[j + count]
+                    };
+
+                    //DateTime timing = startTime;
+
+                    //startTime = startTime.AddMinutes(busStop.TravelTime);
+
+                    sim.Buses[i - 4].BaseSchedule.BusStops.Add(busStop);
+                    //sim.Buses[i - 4].BaseSchedule.TimeStamps.Add(timing); //последний TimeStamp - время возвращения (для зацикливания)
+                }
+                //sim.Buses[i - 4].ReturnTime = stopData[stopData.Count() - 1];
+
+                int minutes = Convert.ToInt32(TimeSpan.Parse(sim.Buses[i - 4].StartTime).TotalMinutes);
+                int currentStation = 0;
+                int stationTotal = sim.Buses[i - 4].BaseSchedule.BusStops.Count();
+                //MessageBox.Show("total:" + stationTotal.ToString());
+
+                DateTime currentTime = Convert.ToDateTime(sim.Buses[i - 4].StartTime);
+                int totalTime = minutes;
+                while (totalTime < 1440)
+                {
+                    BusStop busStop = new BusStop
+                    {
+                        Number = sim.Buses[i - 4].BaseSchedule.BusStops[currentStation].Number,
+                        TravelTime = sim.Buses[i - 4].BaseSchedule.BusStops[currentStation].TravelTime
+                    };
+
+                    DateTime timing = currentTime;
+
+                    sim.Buses[i - 4].FullSchedule.BusStops.Add(busStop);
+
+                    currentTime = currentTime.AddMinutes(busStop.TravelTime);
+                    totalTime += busStop.TravelTime;
+
+                    currentStation += 1;
+                    if (currentStation == stationTotal)
+                    {
+                        currentStation = 0;
+                    }
+
+                    sim.Buses[i - 4].FullSchedule.TimeStamps.Add(timing); //последний TimeStamp - время возвращения (для зацикливания)
+                }
+            }
+
+            //foreach (Bus bus in sim.Buses)
+            //{
+            //    string full = "";
+            //    for (int i = 0; i < bus.FullSchedule.BusStops.Count; i++)
+            //    {
+            //        full += bus.FullSchedule.BusStops[i].Number + " " + bus.FullSchedule.TimeStamps[i].ToString("HH:mm") + "   ";
+            //    }
+
+            //    //foreach (DateTime dt in bus.FullSchedule.TimeStamps)
+            //    //{
+            //    //    full += dt.TimeOfDay + " ";
+            //    //}
+            //    MessageBox.Show(full);
+            //}
+            MessageBox.Show("Файл загружен успешно!", "BusSimulator");
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "BusSimulator");
+            //}
+        }
+
+        AdjacencyGraph<string, Edge<string>> shortestPathGraph;
+        Dictionary<Edge<string>, double> shortestPathCosts;
+        Dictionary<Edge<string>, int> shortestPathBusIDs;
+
+        AdjacencyGraph<string, Edge<string>> cheapestPathGraph;
+        Dictionary<Edge<string>, double> cheapestPathCosts;
+        Dictionary<Edge<string>, int> cheapestPathBusIDs;
+
+        public void SetUpEdgesAndCosts()
+        {
+            shortestPathGraph = new AdjacencyGraph<string, Edge<string>>();
+            shortestPathCosts = new Dictionary<Edge<string>, double>();
+            shortestPathBusIDs = new Dictionary<Edge<string>, int>();
+
+            cheapestPathGraph = new AdjacencyGraph<string, Edge<string>>();
+            cheapestPathCosts = new Dictionary<Edge<string>, double>();
+            cheapestPathBusIDs = new Dictionary<Edge<string>, int>();
+
+            //TimeSpan currentTime = TimeSpan.Parse(cBxTime.Text);
+            foreach (Bus bus in sim.Buses)
+            {
+                int count = (bus.BaseSchedule.BusStops.Count * 2) - 1;
+
+                for (int i = 0; i < count; i++)
+                {
+                    // Граф для самого короткого пути
+                    //if (i == 0 || currentTime == bus.FullSchedule.TimeStamps[i].TimeOfDay) //Добавить следующую остановку если автобус на самой первой или если автобус будет на следующей остановке в нужное время
+                    //{
+                    AddEdgeWithCosts(bus.FullSchedule.BusStops[i].Number.ToString(), bus.FullSchedule.BusStops[i + 1].Number.ToString(), bus.FullSchedule.BusStops[i].TravelTime, shortestPathGraph, shortestPathCosts, shortestPathBusIDs, bus.ID);
+                    //    currentTime = currentTime.Add(bus.FullSchedule.TimeStamps[i].TimeOfDay);
+                    //}
+
+                    for (int j = 0; j < bus.BaseSchedule.BusStops.Count; j++)
+                    {
+                        //if (i == 0 || currentTime == bus.FullSchedule.TimeStamps[i].TimeOfDay)
+                        //{
+                        AddEdgeWithCosts(bus.FullSchedule.BusStops[i].Number.ToString(), bus.FullSchedule.BusStops[j].Number.ToString(), bus.TicketCost, cheapestPathGraph, cheapestPathCosts, cheapestPathBusIDs, bus.ID);
+                        //    currentTime = currentTime.Add(bus.FullSchedule.TimeStamps[i].TimeOfDay);
+                        //}
+                    }
+                }
+            }
+
+
+            //foreach (Edge<string> edge in shortestPathGraph.Edges)
+            //{
+            //    MessageBox.Show(edge.Source.);
+            //}
+
+            //AddEdgeWithCosts("A", "D", 4.0);
+            //AddEdgeWithCosts("C", "B", 1.0);
+        }
+
+        private void AddEdgeWithCosts(string source, string target, double cost, AdjacencyGraph<string, Edge<string>> graph, Dictionary<Edge<string>, double> costs, Dictionary<Edge<string>, int> busIDs, int busID)
+        {
+            var edge = new Edge<string>(source, target);
+            graph.AddVerticesAndEdge(edge);
+            costs.Add(edge, cost);
+            busIDs.Add(edge, busID);
+        }
+
+        private string PrintShortestPath(string @from, string to, AdjacencyGraph<string, Edge<string>> graph, Dictionary<Edge<string>, double> costs, Dictionary<Edge<string>, int> busIDs, int mode)
+        {
+            var edgeCost = AlgorithmExtensions.GetIndexer(costs);
+            var tryGetPath = graph.ShortestPathsDijkstra(edgeCost, @from);
+
+            string type = "";
+            if (mode == 1)
+            {
+                type = "короткий";
+            }
+            else if (mode == 2)
+            {
+                type = "дешёвый";
+            }
+
+            IEnumerable<Edge<string>> path;
+            StringBuilder builder = new StringBuilder();
+
+            if (tryGetPath(to, out path))
+            {
+                builder.Append("Самый " + type + " путь от остановки ");
+                builder.Append(from);
+                builder.Append(" до остановки ");
+                builder.Append(to);
+                builder.Append(":\n");
+                builder.Append("{" + from + "}");
+
+                //MessageBox.Show("Path found from {" + from  + "} to {" + to + "}: {" + from + "}");
+                foreach (var e in path)
+                {
+                    builder.Append(" > {" + e.Target + "}");
+                }
+            }
+            else
+            {
+                builder.Append("Путь не найден.");
+            }
+
+            builder.Append(" на автобусе(ах): ");
+            foreach (Edge<string> edge in path)
+            {
+                foreach (var item in busIDs)
+                {
+                    if (item.Key == edge)
+                    {
+                        //MessageBox.Show(item.Value.ToString());
+                        builder.Append(item.Value.ToString() + " ");
+                    }
+                }
+            }
+
+
+            return builder.ToString();
+        }
+
+        private void btnFindRoutes_Click(object sender, EventArgs e)
+        {
+            //if (cheapestBusID == 0 || fastestBusID == 0)
+            //{
+            //    if (cBxTime.Text.Length < 1)
+            //    {
+            //        lblResult.Text = "Укажите время отправления.";
+            //    }
+            //}
+            //else
+            //{
+            //    if (shortestTime > 0)
+            //    {
+            //        lblResult.Text = "Результат:\nСамый дешёвый автобус: Номер " + cheapestBusID + ", стоимость: " + lowestCost.ToString()
+            //            + "\nСамый быстрый автобус: Номер " + fastestBusID + ", время в пути: " + shortestTime.ToString();
+            //    }
+            //}
+
+            shortestPathGraph = new AdjacencyGraph<string, Edge<string>>();
+            shortestPathCosts = new Dictionary<Edge<string>, double>();
+
+            SetUpEdgesAndCosts();
+
+            lblResult.Text = "Результат:\n" + PrintShortestPath(cBxStart.Text, cBxFinish.Text, shortestPathGraph, shortestPathCosts, shortestPathBusIDs, 1)
+                + "\n" + PrintShortestPath(cBxStart.Text, cBxFinish.Text, cheapestPathGraph, cheapestPathCosts, cheapestPathBusIDs, 2);
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            tBxFileAddress.Text = @"F:\Test.txt";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void cBxStart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cBxTime.Items.Clear();
+            cBxTime.Text = "";
+
+            //Заполнение comboBox'a с временами отправлений
+            for (int i = 0; i < sim.Buses.Count; i++)
+            {
+                if (sim.Buses[i].FullSchedule.BusStops.Any(x => x.Number == Convert.ToInt32(cBxStart.SelectedItem)) == true)
+                {
+                    //int index = 0;
+                    for (int j = 0; j < sim.Buses[i].FullSchedule.BusStops.Count; j++)
+                    {
+                        if (sim.Buses[i].FullSchedule.BusStops[j].Number == Convert.ToInt32(cBxStart.SelectedItem))
+                        {
+                            //index = j;
+                            //}
+                            //else if (sim.Buses[i].FullSchedule.BusStops[j].Number == Convert.ToInt32(cBxFinish.SelectedItem) && index != -2)
+                            //{
+                            string s = sim.Buses[i].FullSchedule.TimeStamps[j].ToString("HH:mm");
+                            cBxTime.Items.Add(s);
+
+                            //    index = -2;
+                        }
+                    }
+                }
+            }
+
+            cBxFinish.Items.Clear();
+            cBxFinish.Text = "";
+
+            for (int i = 1; i <= sim.StopCount; i++)
+            {
+                cBxFinish.Items.Add(i.ToString());
+            }
+
+            cBxFinish.Items.Remove(Convert.ToInt32(cBxStart.SelectedItem));
+        }
+
+        private void cBxFinish_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    //Добавить в отдельный класс в случае дальнейшей доработки, добавить ID где требуется если присоединять базу данных
+    public class Simulation
+    {
+        private List<Bus> _buses = new List<Bus>();
+
+        public IList<Bus> Buses { get { return _buses; } }
+
+        public int StopCount { get; set; }
+    }
+
+    public class Bus
+    {
+        public int ID { get; set; }
+        public string StartTime { get; set; }
+        public int TicketCost { get; set; }
+        public Schedule FullSchedule = new Schedule();
+        public Schedule BaseSchedule = new Schedule();
+    }
+
+    public class BusStop
+    {
+        public int Number { get; set; }// ID
+        public int TravelTime { get; set; }
+    }
+
+    public class Schedule
+    {
+        public List<BusStop> BusStops = new List<BusStop>();
+        public List<DateTime> TimeStamps = new List<DateTime>();
+
+        // private List<BusStop> _busStops = new List<BusStop>();
+        // public IList<BusStop> BusStops { get { return _busStops; } }    
+        // private List<DateTime> _timeStamps = new List<DateTime>();
+        // public IList<DateTime> TimeStamps { get { return _timeStamps; } }
+
+
+    }
+
+}
